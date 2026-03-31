31f2cd544e76f453a4db3d26caae1e9618e66b79
Your Name <you@example.com>
2026-03-31 18:28:40 +0530

    feat: implement google authentication and logout controllers with cookie management

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -14,8 +14,8 @@ export const googleAuth = async (req, res) => {
     let token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(200).json(user);
@@ -26,7 +26,11 @@ export const googleAuth = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    await res.clearCookie("token");
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
     return res.status(200).json({ message: "Logout successfully" });
   } catch (error) {
     return res.status(500).json({ message: `Logout error ${error}` });
